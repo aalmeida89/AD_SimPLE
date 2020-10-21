2a765e01888db2f258fc0d81716981b7534d81b0
alexandre ribeiro de almeida <aalmeida@stone.com.br>
2020-10-21 00:32:51 -0300

    melhorias no script

--- simR.r
+++ simR.r
@@ -3,6 +3,7 @@
 #GoodNews = 0
 #FakeNews = 1
 
+set.seed(10)
 maxLoop <- 1000
 #qual valor vai ser inicializado no top das timeLines
 top <- 1 #Vai começar com FakeNews no top da timeline
@@ -23,14 +24,15 @@ lambda1 <- 0
 #Type = Se é FakeNews(1) ou GoodNews(0)
 #TotalFN = Total de fakeNews no instante Time
 #Time = tempo que chegou o próximo post na timeline, aqui o tempo não é o total, é relativo, ou seja, é o espaço de tempo entre o ultimo post e a hora desse post
-colname <- c("TlSource", "TlDestination", "Type", "TotalFN", "Time")
+colname <- c("TlSource", "TlDestination", "Type", "TotalFN", "TotalTime", "RelativeTime")
 
-#crio as timelines para cada usuario
-timeLine <- matrix(rep_len(c(top,botton), K*N), nrow = N, byrow = T)
-colnames(timeLine) <- c("Top", "Botton")
+#Timeline é criada da seguinte forma: [top, 0, 0, ..., 0, 0, booton] para cada linha, ou seja, se top = 1 e botton = 1, teremos todas as linhas como [1,0,...,0,1], timeline começa com fakenews e termina com fakenews
+timeLine <- matrix(rep(c(top, rep(0,K-2), botton),N), nrow = N, byrow = T)
+colnames(timeLine) <- c("Top", rep("middle", K-2), "Botton")
 
 #crio minha matriz de eventos
 eventos <- matrix(rep(0,length(colname)), ncol = length(colname))
+eventos[4] <- Reduce(`+`, (colSums(timeLine)))
 colnames(eventos) <- colname
 
 #cria matriz para receber o proximo evento
@@ -39,7 +41,7 @@ colnames(prox_evento) <- colname
 
 
 #calcula o proximo evento
-calculaProxEvento <- function() {
+calculaProxEvento <- function(tl) {
 	#inicializo uma matriz para inserir os eventos que vão ser gerados para poder pegar o proximo evento, sendo que esse evento é o com menor tempo (por falta de memoria podemos fazer assim)
 	prox <- matrix(rep(0,), ncol = length(colname), nrow = N)
 	colnames(prox) <- colname
@@ -51,10 +53,18 @@ calculaProxEvento <- function() {
 		#popula meus dados
 		prox[i,1] <- i #TlSource
 		prox[i,2] <- vizinho #TlDestination
-		prox[i,3] <- sample(0:1,1) #Type # 0 = GN, 1 = FN #aqui ta prob 1/2, tem que alterar depois, esta assim por questão de testes
+		
+		if(rowSums(tl)[i] == K){#todos os posts do propagador sao FakeNews, entao ele propaga FN
+		  prox[i,3] <- 1
+		} else if(rowSums(tl)[i] == 0){#todos os posts do propagador sao GoodNews, entao ele propaga GN
+		  prox[i,3] <- 0
+		} else {
+		  prox[i,3] <- sample(0:1,1) #Type # 0 = GN, 1 = FN #aqui ta prob 1/2, tem que alterar depois, esta assim por questão de testes
+		}
 		##prox[i,3] <- if(sample(0:4,1) > 3) {0} else {1} #Aqui, por questão de testes tmb, coloquei uma prob de 3/5 para receber GoodNews e 2/5 para FakeNews.
-		prox[i,4] <- 0 #Total_FakeNews, vai ser calculado depois, coloco 0 para não ficar vazio
-		prox[i,5] <- rexp(1,1) #Time #tempo exponencial usando função do próprio R.
+		#prox[i,4] <- 0 #Total_FakeNews, vai ser calculado depois, coloco 0 para não ficar vazio
+		prox[i,6] <- rexp(1,1) #Time #tempo exponencial usando função do próprio R.
+		
 	}
 	
 	#orderna evento pela coluna "Time" em ordem crescente e adiciona esse evento no proximo evento.
@@ -70,14 +80,25 @@ l<-0
 #Meu loop roda 1000 vezes se minha timeLine não estiver totalmente com GoodNews ou totalmente com FakeNews, caso tenham todas as timeLines com FakeNews ou GoodNews, ele para
 while( (l <= maxLoop) & (Reduce(`+`, (colSums(timeLine))) != 0) &  (Reduce(`+`, (colSums(timeLine))) != K*N) ){
 	#uso a function para calcular o proximo evento
-	prox_evento <- calculaProxEvento()
+	prox_evento <- calculaProxEvento(timeLine)
 
-	#faço a rotação da timeLine, ou seja, o que está no top vai pro botton e o top recebe o próximo post que pode ser FN ou GN
-	timeLine[prox_evento[2], 2] <- timeLine[prox_evento[2], 1] #top vai pra botton
+	#faço a rotação da timeLine, ou seja, o top recebe o próximo post que pode ser FN ou GN e os outros posts da timeline "descem".
+	for(i in K:2){
+		timeLine[prox_evento[2], i] <- timeLine[prox_evento[2], i-1] #top vai pra botton
+	}
 	timeLine[prox_evento[2], 1] <- prox_evento[3] #evento entra no top da timeline
+	
+	######################################################################
+	## RDN Case, mas validar probabilidade se eh linearmente distribuido ##
+	######################################################################
+	#para RDN podemos escolher aleatoriamente onde a mensagem vai entrar, entao basicamente vamos soh substituir um post aleatoriamente
+	#timeLine[prox_evento[2], sample(1:K,1)] <- prox_evento[3] #top vai pra botton
 	
 	#calcula quantas FakeNews tem no total e adiciono na coluna correta
 	prox_evento[4] <- Reduce(`+`, (colSums(timeLine)))
+	#adiciona tempo total ate o momento
+	prox_evento[5] <- colSums(eventos)[6]+prox_evento[6]
+	
 	
 	#rbind serve para adicionar uma row na matriz, ou seja, estou adicionando o prox_evento na minha matriz de eventos
 	eventos <- rbind(eventos, prox_evento)
@@ -89,3 +110,5 @@ while( (l <= maxLoop) & (Reduce(`+`, (colSums(timeLine))) != 0) &  (Reduce(`+`, 
 timeLine
 #quantos eventos foram gerados
 nrow(eventos)
+#plota a quantodade de FakeNews por evento
+plot(eventos[,4], )
